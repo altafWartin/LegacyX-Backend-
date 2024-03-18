90d03f935d7054af29a19c3f2c303d67378b5d46
altafWartin <157889037+altafWartin@users.noreply.github.com>
2024-03-18 16:55:37 +0530

    mdb link change

--- database/index.js
+++ database/index.js
@@ -3,7 +3,7 @@ const { MONGODB_CONNECTION_STRING } = require("../config/index");
 
 const dbConnect = async () => {
   try {
-    const con = await mongoose.connect(MONGODB_CONNECTION_STRING, {
+    const con = await mongoose.connect("mongodb+srv://legacyempire1:Legacy140399@cluster0.9qqagie.mongodb.net/", {
       dbName: "legacyProd",
     });
     console.log(`Database connected to host: ${con.connection.host}`);
