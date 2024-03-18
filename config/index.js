5ea41d0982970994e3e25f6048760fddf1f6a8e7
altafWartin <157889037+altafWartin@users.noreply.github.com>
2024-03-19 02:57:13 +0530

    chang files

--- config/index.js
+++ config/index.js
@@ -1,16 +1,16 @@
 const dotenv = require("dotenv").config();
 
-const PORT = process.env.PORT || 3000;
-const MONGODB_CONNECTION_STRING = process.env.MONGODB_CONNECTION_STRING || "mongodb://localhost:27017/mydatabase";
+const PORT = process.env.PORT || 4400;
+const MONGODB_CONNECTION_STRING = process.env.MONGODB_CONNECTION_STRING || "mongodb+srv://legacyempire1:Legacy140399@cluster0.9qqagie.mongodb.net/";
 const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET || "default_access_token_secret";
 const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET || "default_refresh_token_secret";
-const BACKEND_SERVER_PATH = process.env.BACKEND_SERVER_PATH || "http://localhost:3000";
+// const BACKEND_SERVER_PATH = process.env.BACKEND_SERVER_PATH || "http://localhost:3000";
 
 module.exports = {
   PORT,
   MONGODB_CONNECTION_STRING,
   ACCESS_TOKEN_SECRET,
   REFRESH_TOKEN_SECRET,
-  BACKEND_SERVER_PATH,
+  // BACKEND_SERVER_PATH,
 };
      
