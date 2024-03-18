35cef4787ae632a2715670061d84b08f15bb1cb2
altafWartin <157889037+altafWartin@users.noreply.github.com>
2024-03-19 03:07:30 +0530

    new change

--- config/index.js
+++ config/index.js
@@ -1,16 +1,15 @@
 const dotenv = require("dotenv").config();
-
-// const PORT = process.env.PORT || 4400;
-const MONGODB_CONNECTION_STRING = process.env.MONGODB_CONNECTION_STRING || "";
-const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET || "default_access_token_secret";
-const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET || "default_refresh_token_secret";
-// const BACKEND_SERVER_PATH = process.env.BACKEND_SERVER_PATH || "http://localhost:3000";
-
-module.exports = {
-  // PORT, 
-  MONGODB_CONNECTION_STRING,
-  ACCESS_TOKEN_SECRET,
-  REFRESH_TOKEN_SECRET,
-  // BACKEND_SERVER_PATH,
-};
-     
+  
+      // const PORT = process.env.PORT;
+      const MONGODB_CONNECTION_STRING = process.env.MONGODB_CONNECTION_STRING;
+      const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
+      // const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
+      // const BACKEND_SERVER_PATH = process.env.BACKEND_SERVER_PATH;
+      
+      module.exports = {
+        // PORT,
+        MONGODB_CONNECTION_STRING,
+        ACCESS_TOKEN_SECRET,
+        // REFRESH_TOKEN_SECRET,
+        // BACKEND_SERVER_PATH,
+      }; 
